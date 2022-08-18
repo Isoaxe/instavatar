b3dd7442cd5a875ccd77ae3366b776ebed9c2ce8
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-08-18 21:33:46 +0100

    app: Set api url dynamically
    Select the api url based on whether the emulator is running or not.

--- public/app.js
+++ public/app.js
@@ -3,6 +3,13 @@ let btn = document.querySelector('#get_avatar')
 let img = document.querySelector('#result')
 let cache_info = document.querySelector('#cache_info')
 
+// Set API_URL based on whether locally hosted emulator is running or not.
+const localApiUrl = "http://localhost:5001/insta-profile-pic/us-central1/instapic";
+const remoteApiUrl = "https://us-central1-insta-profile-pic.cloudfunctions.net/instapic";
+const local = window.location.hostname === "localhost"; // true if local
+
+const API_URL = local ? localApiUrl : remoteApiUrl;
+
 btn.addEventListener('click', async e => {
   let user = username.value
   let result = await fetch(`/api/avatar?username=${user}`)
