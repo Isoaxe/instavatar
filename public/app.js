4c2c9ac0de0a5c4078060c1665529abc27e953ed
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-09-06 23:00:04 +0700

    app: Add semi-colons
    Add semi-colons for consistency. Also add a space for more logical separation of code.
    Will add Eslint in due course.

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
-let username = document.querySelector('#username')
-let btn = document.querySelector('#get_avatar')
-let img = document.querySelector('#result')
+let username = document.querySelector('#username');
+let btn = document.querySelector('#get_avatar');
+let img = document.querySelector('#result');
 
 // Set API_URL based on whether locally hosted emulator is running or not.
 // TODO: Set both api urls to the values associated with your project.
@@ -10,6 +10,7 @@ const localPicUrl = "http://localhost:9199/insta-profile-pic.appspot.com/avatars
 const remotePicUrl = "gs://insta-profile-pic.appspot.com/avatars/";
 const localProxyUrl = "http://localhost:5002/private-cors-server/us-central1/proxy/";
 const remoteProxyUrl = "https://private-cors-server.herokuapp.com/";
+
 const local = window.location.hostname === "localhost"; // true if local
 
 const API_URL = local ? localApiUrl : remoteApiUrl;
@@ -17,8 +18,8 @@ const PIC_URL = local ? localPicUrl : remotePicUrl;
 const PROXY_URL = local ? localProxyUrl : remoteProxyUrl;
 
 btn.addEventListener('click', async e => {
-  let user = username.value
+  let user = username.value;
   let result = await fetch(`${PROXY_URL}${API_URL}?username=${user}`);
-  username.value = null
+  username.value = null;
   img.src = PIC_URL + user + ".png";
 })
