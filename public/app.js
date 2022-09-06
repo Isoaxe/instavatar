ccd0306cdb59c59cde0b2fd8cf92e2ecf216135d
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-09-06 22:42:56 +0700

    app: Add url selector for cors proxy url
    Add a local and hosted url for the CORS proxy server, as with the api and pic urls. These are for development and production respectively.

--- public/app.js
+++ public/app.js
@@ -8,11 +8,14 @@ let cache_info = document.querySelector('#cache_info')
 const localApiUrl = "http://localhost:5001/insta-profile-pic/us-central1/instapic";
 const remoteApiUrl = "https://us-central1-insta-profile-pic.cloudfunctions.net/instapic";
 const localPicUrl = "http://localhost:9199/insta-profile-pic.appspot.com/avatars/";
-const remotePicUrl = "gs://insta-profile-pic.appspot.com/avatars/"
+const remotePicUrl = "gs://insta-profile-pic.appspot.com/avatars/";
+const localProxyUrl = "http://localhost:5002/private-cors-server/us-central1/proxy/";
+const remoteProxyUrl = "https://private-cors-server.herokuapp.com/";
 const local = window.location.hostname === "localhost"; // true if local
 
 const API_URL = local ? localApiUrl : remoteApiUrl;
 const PIC_URL = local ? localPicUrl : remotePicUrl;
+const PROXY_URL = local ? localProxyUrl : remoteProxyUrl;
 
 btn.addEventListener('click', async e => {
   let user = username.value
