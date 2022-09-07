57ebfa7a360046fa6af0b016ef60eb84aa929d59
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-09-07 11:41:36 +0700

    index: Remove the code that gets session cookie
    This code was commented out anyways. No longer required to login, get cookie or access cache.

--- functions/index.mjs
+++ functions/index.mjs
@@ -67,16 +67,6 @@ async function getProfilePicUrl(user) {
     return data.profile_pic_url_hd;
   }
 
-  /*
-  // With this approach, a login to Instagram is no longer required.
-  // Check if session exists or not.
-  let sessionCookie = await getSessionCache();
-  if (!sessionCookie) {
-    sessionCookie = await login(username, password);
-    await setSessionCache(sessionCookie);
-  }
-  */
-
   // profile_pic_url_hd can be parsed from user html page itself or from Public api.
   // Public api needs more testing.
   // Try with Public api first, fallback to page parsing after.
