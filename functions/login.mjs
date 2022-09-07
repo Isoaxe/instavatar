e10be3ce8275e475540997150c92c3113d8bc8fa
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-09-07 11:47:01 +0700

    login: Add getSessionCookie method
    This wraps the code that was removed from index in the previous commit in a function.
    This way it can be more easily called if this functionality is to be returned in the future.

--- functions/login.mjs
+++ functions/login.mjs
@@ -8,3 +8,13 @@ const loginUrl = "https://www.instagram.com/accounts/login";
 // The userAgent does NOT need to be changed. Any valid userAgent will do.
 const userAgent =
   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36";
+
+// Check if session exists or not.
+async function getSessionCookie() {
+  let sessionCookie = await getSessionCache();
+  if (!sessionCookie) {
+    sessionCookie = await login(username, password);
+    await setSessionCache(sessionCookie);
+  }
+  return sessionCookie;
+}
